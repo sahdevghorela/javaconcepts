0b356e75ed78a3efb5ce9d4957c09282a6aedd1b
sahdev <sahdevghorela37@gmail.com>
2017-01-28 19:01:22 +0100

    Interface default methods demo

---
+++ javaeight/src/main/java/java8/defaultMethods/CompanyEmployee.java
@@ -0,0 +1,28 @@
+package java8.defaultMethods;
+
+public class CompanyEmployee implements Company,Employee{
+
+    /*
+    -We can not have multiple implementations of interfaces with same default methods.
+    - As an alternative we can override default method and reuse super methods.
+     */
+    @Override
+    public String getName() {
+        return Company.super.getName() + Employee.super.getName();
+    }
+}
+
+
+
+interface Company{
+
+    default String getName(){
+        return "Company Name";
+    }
+}
+
+interface  Employee{
+    default String getName(){
+        return "";
+    }
+}
